cb98bb3477368a19f14eb4ef16c8314c3a6a543d
saanviGarg01 <158058912+saanviGarg01@users.noreply.github.com>
2024-02-05 18:47:40 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,19 @@
+import firebase from "firebase";
+require("@firebase/firestore");
+
+//Paste your firebaseConfig here
+const firebaseConfig = {
+    apiKey: "AIzaSyA17SLIOnZVtSDu_ShAxvxKA-7gVdivDHQ",
+    authDomain: "e-ride-cycle.firebaseapp.com",
+    projectId: "e-ride-cycle",
+    storageBucket: "e-ride-cycle.appspot.com",
+    messagingSenderId: "53096234986",
+    appId: "1:53096234986:web:438891ebf08622970bbc93"
+  };
+
+
+firebase.initializeApp(firebaseConfig);
+
+export default firebase.firestore();
+
+
